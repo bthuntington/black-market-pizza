e11cfb8c520f102be753f704eaf75b5d784549e2
Qwertie64982 <maxwell.a.sherman@live.com>
2019-04-22 16:39:33 -0700

    Clean up login page and set it as root, tinker with database

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
+# https://guides.rubyonrails.org/routing.html
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'welcome' => 'pages#home'
-  #get 'login' => 'pages#login'
-  get "/Desktop/black-market-pizza/app/views/pages/login.html.erb", to: "pages#login", as: "login"
+  root 'pages#login'
+  # get 'welcome' => 'pages#login'
+  get 'test' => 'pages#test'
 end
