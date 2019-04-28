e6d6fe3c639996943dc1fc986ab0528c62520c58
bthuntington <bhuntington@zagmail.gonzaga.edu>
2019-04-28 16:47:11 -0700

    user change

--- db/migrate/20190406233645_create_users.rb
+++ db/migrate/20190406233645_create_users.rb
@@ -2,6 +2,9 @@ class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       add_column :users, :passwords
+      t,string "name"
+      t.integer "balance"
+      t.integer "user_id"
       t.timestamps
     end
   end
