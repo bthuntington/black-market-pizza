e150840132096f3e506ceb950263866f1159f1ed
bthuntington <bhuntington@zagmail.gonzaga.edu>
2019-04-29 15:40:18 -0700

    new database stuff

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_28_205819) do
+ActiveRecord::Schema.define(version: 2019_04_29_223906) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 2019_04_28_205819) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "balance"
+    t.string "integer"
+    t.integer "userId"
+    t.string "username"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
